594baa9cbdf900edda7274a7ce51ff7affad4afe
max16code <amanzeikedi16@gmail.com>
2025-06-02 14:09:48 +0100

    new pushing

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -120,7 +120,7 @@ export default function RegisterPage() {
             </div>
                 {errors.apiError &&( <p className='text-sm text-red-400 font-semibold'> {errors.apiError}</p>)} 
 
-            <button className="bg-green-600 text-white px-4 py-2">Submit</button>
+            <button className="bg-pink-800 text-white px-4 py-2">Submit</button>
 
         </form>
     )
